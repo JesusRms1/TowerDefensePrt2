fb088ee53ca2309c3954d35a49d0d106a4cde7bc
Jesus Ramos <jesusri902@gmail.com>
2025-04-10 19:57:05 -0700

    Part3
    part3

--- Tower Defense/Assets/Scripts/BuildManager.cs
+++ Tower Defense/Assets/Scripts/BuildManager.cs
@@ -1,4 +1,3 @@
-
 using UnityEngine;
 
 public class BuildManager : MonoBehaviour
@@ -7,7 +6,7 @@ public class BuildManager : MonoBehaviour
 
     void Awake()
     {
-        if(instance != null)
+        if (instance != null)
         {
             Debug.LogError("More than one BuildManager in the scene");
             return;
@@ -16,34 +15,45 @@ public class BuildManager : MonoBehaviour
     }
 
     public GameObject buildEffect;
-
-
-
+    public GameObject sellEffect;
 
     private TurretBlueprint turretToBuild;
+    private Node selectedNode;
+
+    public NodeUI nodeUI;
 
     public bool CanBuild { get { return turretToBuild != null; } }
     public bool HasMoney { get { return PlayerStats.Money >= turretToBuild.cost; } }
-    public void BuildTurretOn(Node node)
+
+    
+    public void SelectNode(Node node)
     {
 
-        if (PlayerStats.Money < turretToBuild.cost)
+        if (selectedNode == node)
         {
-            Debug.Log("Not enough money to build that!");
+            DeselectNode();
             return;
         }
 
-        PlayerStats.Money -= turretToBuild.cost;
-        GameObject turret = (GameObject)Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
-        node.turret = turret;
+        selectedNode = node;
+        turretToBuild = null;
+        nodeUI.SetTarget(node);
+    }
 
-        GameObject effect = (GameObject)Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity);
-        Destroy(effect, 5f);
-        Debug.Log("Turret build! Money left: " + PlayerStats.Money);
+    public void DeselectNode()
+    {
+        selectedNode = null;
+        nodeUI.Hide();
     }
 
     public void SelectTurretToBuild(TurretBlueprint turret)
     {
         turretToBuild = turret;
+        DeselectNode();
+    }
+
+    public TurretBlueprint GetTurretToBuild()
+    {
+        return turretToBuild;
     }
 }
--- Tower Defense/Assets/Scripts/CameraController.cs
+++ Tower Defense/Assets/Scripts/CameraController.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private bool doMovemnet = true;
+    
     public float panSpeed = 30f;
     public float panBorderThickness = 10f;
 
@@ -17,10 +17,7 @@ public class CameraController : MonoBehaviour
             this.enabled = false;
             return;
         }
-        if (Input.GetKeyDown(KeyCode.Escape))
-            doMovemnet = !doMovemnet;
-        if (!doMovemnet)
-            return;
+     
 
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
